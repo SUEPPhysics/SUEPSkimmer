068824d75d572d49bafb5a9aba8685e49627a36f
Chris Papageorgakis <Christos.Papageorgakis@cern.ch>
2023-05-01 19:46:59 +0200

    include gfal as alternative

--- data/countFilesMIT.sh
+++ data/countFilesMIT.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 inputFile=$1
+use_xrootd=true
 
 while read p
 do
   echo -n "${p:28:-11}: "
-  xrdfs root://xrootd.cmsaf.mit.edu/ ls $p | grep ".root" | wc -l
-done<${inputFile}
+  if [ $use_xrootd ] ; then
+    xrdfs root://xrootd.cmsaf.mit.edu/ ls $p | grep ".root" | wc -l
+  else
+    gfal-ls gsiftp://se01.cmsaf.mit.edu:2811//cms/${p} | grep ".root" | wc -l
+  fi
+done < ${inputFile}
 
 
