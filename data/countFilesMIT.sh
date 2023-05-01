e4eb7da401127d1b905dcb06f1214ffcac9c3f4a
Chris Papageorgakis <Christos.Papageorgakis@cern.ch>
2023-05-01 23:33:45 +0200

    fix conditional

--- data/countFilesMIT.sh
+++ data/countFilesMIT.sh
@@ -12,7 +12,7 @@ do
     xrdfs root://xrootd.cmsaf.mit.edu/ ls $p | grep ".root" | wc -l
   elif [ "$engine" = gfal ] ; then
     { gfal-ls gsiftp://se01.cmsaf.mit.edu:2811//cms/$p | grep ".root" | wc -l; } 2> /dev/null
-    while ${PIPESTATUS[0]} ; do
+    while [ ${PIPESTATUS[0]} -ne 0 ] ; do
       sleep 1
       { gfal-ls gsiftp://se01.cmsaf.mit.edu:2811//cms/$p | grep ".root" | wc -l; } 2> /dev/null
     done
