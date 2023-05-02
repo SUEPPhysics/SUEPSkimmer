6e5d43036edfb4817006f8915fd6dc1c621ffe7a
Chris Papageorgakis <Christos.Papageorgakis@cern.ch>
2023-05-02 15:03:51 +0200

    Counting script is not affected by errors anymore

--- data/countFilesMIT.sh
+++ data/countFilesMIT.sh
@@ -5,19 +5,23 @@
 inputFile=$1 # datasets.txt
 engine=$2 # xrootd or gfal
 
-while read p
-do
+while read p; do
   echo -n "${p:28:-11}: "
   if [ "$engine" = xrootd ] ; then
-    xrdfs root://xrootd.cmsaf.mit.edu/ ls $p | grep ".root" | wc -l
-  elif [ "$engine" = gfal ] ; then
-    { gfal-ls gsiftp://se01.cmsaf.mit.edu:2811//cms/$p | grep ".root" | wc -l; } 2> /dev/null
-    while [ ${PIPESTATUS[0]} -ne 0 ] ; do
+    files=( $({ xrdfs root://xrootd.cmsaf.mit.edu/ ls $p | grep ".root"; } 2> /dev/null) )
+    while [ $PIPESTATUS -ne 0 ]; do
       sleep 1
-      { gfal-ls gsiftp://se01.cmsaf.mit.edu:2811//cms/$p | grep ".root" | wc -l; } 2> /dev/null
+      files=( $({ xrdfs root://xrootd.cmsaf.mit.edu/ ls $p | grep ".root"; } 2> /dev/null) )
+    done
+  elif [ "$engine" = gfal ] ; then
+    files=( $({ gfal-ls gsiftp://se01.cmsaf.mit.edu:2811//cms/$p | grep ".root"; } 2> /dev/null) )
+    while [ $PIPESTATUS -ne 0 ]; do
+      sleep 1
+      files=( $({ gfal-ls gsiftp://se01.cmsaf.mit.edu:2811//cms/$p | grep ".root"; } 2> /dev/null) )
     done
   else
     echo "Unknown engine: $engine"
     break
   fi
+  echo ${#files[@]}
 done < ${inputFile}
