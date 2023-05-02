8bd48069bf804505cd5a934dea6f72b1712775e8
Chris Papageorgakis <Christos.Papageorgakis@cern.ch>
2023-05-02 15:26:12 +0200

    update filename dumping script

--- data/dumpFilenames.sh
+++ data/dumpFilenames.sh
@@ -1,15 +1,35 @@
 #!/bin/sh
 
-inputFile=${1}
-allFiles=allFIles_${inputFile:0:-4}.txt
+# Usage: ./dumpFilenames.sh datasets.txt xrootd
 
-if [ -f "${allFiles}" ]
+inputFile=$1
+engine=$2
+
+out_dir="filenames"
+
+if [ -f "${out_dir}" ]
 then
-  rm ${allFiles}
+  rm ${out_dir}
 fi
 
-while read p
-do
-  xrdfs root://xrootd.cmsaf.mit.edu/ ls $p | grep ".root" >> ${allFiles}
+while read p; do
+  file_out="${p:28}.txt"
+  if [ "$engine" = xrootd ] ; then
+    files=( $({ xrdfs root://xrootd.cmsaf.mit.edu/ ls $p | grep ".root"; } 2> /dev/null) )
+    while [ $PIPESTATUS -ne 0 ]; do
+      sleep 1
+      files=( $({ xrdfs root://xrootd.cmsaf.mit.edu/ ls $p | grep ".root"; } 2> /dev/null) )
+    done
+  elif [ "$engine" = gfal ] ; then
+    files=( $({ gfal-ls gsiftp://se01.cmsaf.mit.edu:2811//cms/$p | grep ".root"; } 2> /dev/null) )
+    while [ $PIPESTATUS -ne 0 ]; do
+      sleep 1
+      files=( $({ gfal-ls gsiftp://se01.cmsaf.mit.edu:2811//cms/$p | grep ".root"; } 2> /dev/null) )
+    done
+  else
+    echo "Unknown engine: $engine"
+    break
+  fi
+  echo ${files[@]} > ${file_out}
 done < ${inputFile}
 
