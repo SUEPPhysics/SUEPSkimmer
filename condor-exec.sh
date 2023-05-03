38d027a3a9244953d20163c8b744ce1631222e29
Chris Papageorgakis <Christos.Papageorgakis@cern.ch>
2023-05-03 17:31:14 +0200

    one input file per job for easier counting

--- condor-exec.sh
+++ condor-exec.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
-echo "Starting job on " `date` #Date/time of start of job
-echo "Running on: `uname -a`" #Condor job is running on this node
-echo "System software: `cat /etc/redhat-release`" #Operating System on that node
+echo "Starting job on " $(date) # Date/time of start of job
+echo "Running on: $(uname -a)" # Condor job is running on this node
+echo "System software: $(cat /etc/redhat-release)" # Operating System on that node
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
 # Input section
 cmssw=$1
 dataset=$2
-input_file=$3
+number=$3
 protocol=$4
 
 tar -xf ${cmssw}.tgz
 rm ${cmssw}.tgz
 export SCRAM_ARCH=slc7_amd64_gcc820
 cd ${cmssw}/src
-eval `scramv1 runtime -sh` # cmsenv is an alias not on the workers
+eval $(scramv1 runtime -sh) # cmsenv is an alias not on the workers
 cd SUEPSkimmer
 source compile.sh
 
 # Get input files
+input_file=$(sed "${number}q;d" data/filenames/${dataset}.txt)
+input_path="/store/user/paus/nanosu/A02/${dataset}/${input_file}"
 if [ "${protocol}" = xrootd ] ; then
-    until xrdcp root://xrootd.cmsaf.mit.edu//store/user/paus/nanosu/A01/${input_file} ${input_file}; do
+    until xrdcp root://xrootd.cmsaf.mit.edu/${input_path} ${input_file}; do
         sleep 1
     done
 elif [ "${protocol}" = gfal ] ; then
-    until gfal-cp gsiftp://se01.cmsaf.mit.edu:2811//cms//store/user/paus/nanosu/A01/${input_file} ${input_file}; do
+    until gfal-cp gsiftp://se01.cmsaf.mit.edu:2811//cms/${input_path} ${input_file}; do
         sleep 1
     done
 else
@@ -32,12 +34,13 @@ else
     exit 1
 fi
 
-./bin/skimmer ${output} ${input_file}
+./bin/skimmer output.root ${input_file}
 
-until xrdcp -f ${output} root://cmseos.fnal.gov//store/user/lpcsuep/SUEPNano_skimmed/${dataset}/${output}; do
+output_path="/store/user/lpcsuep/SUEPNano_skimmed/${dataset}/${input_file}"
+until xrdcp -f output.root root://cmseos.fnal.gov/${output_path}; do
     sleep 1
 done
 
-rm ${ouput} ${input_file}
+rm output.root ${input_file}
 cd ${_CONDOR_SCRATCH_DIR}
 rm -rf ${cmssw}
--- condorSubmitter.sh
+++ condorSubmitter.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/sh
 
+# Usage: ./condorSubmitter.sh datasets.txt protocol prepare_only
+
 source ${PWD}/prepareCondor.sh
 
 datasets=$1
@@ -13,24 +15,24 @@ mkdir condor
 
 while read p; do
   nfiles=$(wc -l data/filenames/${p}.txt)
-  namestring=${p:28:-4}
-  argument="${CMSSW_VERSION} ${namestring} \$(Process)"
+  dataset=${p:28:-4}
+  argument="${CMSSW_VERSION} ${dataset} \$(Process) ${protocol}"
 
   # Write jdl file
-  echo "universe = vanilla" >> condor/${namestring}.jdl
-  echo "Executable = condor-exec.sh" >> condor/${namestring}.jdl
-  echo "Should_Transfer_Files = YES" >> condor/${namestring}.jdl
-  echo "WhenToTransferOutput = ON_EXIT" >> condor/${namestring}.jdl
-  echo "Transfer_Input_Files = condor-exec.sh, ${CMSSW_VERSION}.tgz" >> condor/${namestring}.jdl
-  echo "Arguments = ${argument}" >> condor/${namestring}.jdl
-  echo "Output = condor/logs/skimmer_${namestring}_\$(Cluster)_\$(Process).stdout" >> condor/${namestring}.jdl
-  echo "Error = condor/logs/skimmer_${namestring}_\$(Cluster)_\$(Process).stderr" >> condor/${namestring}.jdl
-  echo "Log = condor/logs/skimmer_${namestring}_\$(Cluster)_\$(Process).log" >> condor/${namestring}.jdl
-  echo "x509userproxy = \$ENV(X509_USER_PROXY)" >> condor/${namestring}.jdl
-  echo "Queue ${nfiles}" >> condor/${namestring}.jdl
+  echo "universe = vanilla" >> condor/${dataset}.jdl
+  echo "Executable = condor-exec.sh" >> condor/${dataset}.jdl
+  echo "Should_Transfer_Files = YES" >> condor/${dataset}.jdl
+  echo "WhenToTransferOutput = ON_EXIT" >> condor/${dataset}.jdl
+  echo "Transfer_Input_Files = condor-exec.sh, ${CMSSW_VERSION}.tgz" >> condor/${dataset}.jdl
+  echo "Arguments = ${argument}" >> condor/${dataset}.jdl
+  echo "Output = condor/logs/skimmer_${dataset}_\$(Cluster)_\$(Process).stdout" >> condor/${dataset}.jdl
+  echo "Error = condor/logs/skimmer_${dataset}_\$(Cluster)_\$(Process).stderr" >> condor/${dataset}.jdl
+  echo "Log = condor/logs/skimmer_${dataset}_\$(Cluster)_\$(Process).log" >> condor/${dataset}.jdl
+  echo "x509userproxy = \$ENV(X509_USER_PROXY)" >> condor/${dataset}.jdl
+  echo "Queue ${nfiles}" >> condor/${dataset}.jdl
 
   # Submit job
-  if [ "$prepare_only" = 0 ] ; then
-    condor_submit condor/${namestring}.jdl
+  if [ "${prepare_only}" = 0 ] ; then
+    condor_submit condor/${dataset}.jdl
   fi
-done
+done < ${datasets}
